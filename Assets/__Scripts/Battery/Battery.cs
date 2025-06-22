99323de1af7fff39b0e85880defcf6668ff62099
SergeyGT <sergeyburdenyuk@mail.ru>
2025-06-22 17:58:47 +0300

    Добавлен скрипт для батарейки

---
+++ Assets/__Scripts/Battery/Battery.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class Battery : MonoBehaviour, IInteractableObject
+{
+    [Header("Battery Settings")]
+    [SerializeField] private float _capacity = 40;
+
+    public static Action<float> GiveCharge;
+
+    public void Interact()
+    {
+        GiveCharge?.Invoke(_capacity);
+    }
+}
--- Assets/__Scripts/Character/Flashlight/Flashlight.cs
+++ Assets/__Scripts/Character/Flashlight/Flashlight.cs
@@ -2,21 +2,50 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
-    [SerializeField] private GameObject flashlight;
+    [SerializeField] private GameObject flashlight;    
     private bool isActive = false;
+
+    [Header("Flashlight Settings")]
+    [SerializeField] private float _capacity = 100f;
+    [SerializeField] private float _chargeTime = 0.5f;
+
+    private const float ACUM_SIZE = 100f;
+    
     private void Start()
     {
         if (flashlight == null) Debug.LogError("Flashlight is NULL!");
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        Battery.GiveCharge += TakeCharge;
+    }
+
+    private void OnDisable()
+    {
+        Battery.GiveCharge -= TakeCharge;
+    }
+
+    private void TakeCharge(float amount)
+    {
+        _capacity += amount;
+        _capacity = Mathf.Min(_capacity, ACUM_SIZE);
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse1))
+        // не включаем фонарь если заряда нет
+        if (Input.GetKeyDown(KeyCode.Mouse1) && _capacity != 0)
         {
             isActive = !isActive;
 
             flashlight.SetActive(isActive);
+        }
+
+        // если фонарь работал, но заряда больше нет, он выкючается автоматически
+        if (_capacity == 0)
+        {
+            flashlight.SetActive(false);
         }
 
     }
