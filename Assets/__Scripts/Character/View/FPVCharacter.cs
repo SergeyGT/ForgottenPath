133e105212559202839089470750b335c116fd82
SergeyGT <sergeyburdenyuk@mail.ru>
2025-06-22 13:27:36 +0300

    Реализована камера от первого лица

---
+++ Assets/__Scripts/Character/View/FPVCharacter.cs
@@ -0,0 +1,29 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class FPVCharacter : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera cinemachineCamera;
+
+    private float rotationX = 0f;
+    private float rotationY = 0f;
+
+    public float sensivity;
+
+    private void Start()
+    {
+        if (sensivity == null || sensivity == 0) sensivity = 1;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensivity;
+
+        rotationY += mouseX;
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        cinemachineCamera.transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0);
+    }
+}
