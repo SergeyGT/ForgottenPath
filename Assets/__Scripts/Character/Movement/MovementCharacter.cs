d41a5c9c7adefce90f9240970e56f44ebf60f376
SergeyGT <sergeyburdenyuk@mail.ru>
2025-06-22 14:10:46 +0300

    Update: MovementCharacter

--- Assets/__Scripts/Character/Movement/MovementCharacter.cs
+++ Assets/__Scripts/Character/Movement/MovementCharacter.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class MovementCharacter : MonoBehaviour
 {
+    [Header("Movement Settings")]
     [SerializeField] private int _speed = 1;
     [SerializeField] private int _shiftSpeed = 5;
+    [SerializeField] private float _acceleration = 0.2f;
+    [SerializeField] private float _deceleration = 0.1f;
     private Rigidbody _rigidbody;
     private Vector3 _currentVelocity;
     private Vector3 _targetVelocity;
@@ -29,9 +32,9 @@ public class MovementCharacter : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _currentVelocity = Vector3.Lerp(_currentVelocity, _targetVelocity, Time.fixedDeltaTime / 0.2f);
+        float lerpTime = (_targetVelocity == Vector3.zero) ? _deceleration : _acceleration;
+        _currentVelocity = Vector3.Lerp(_currentVelocity, _targetVelocity, Time.fixedDeltaTime / lerpTime);
 
-        Vector3 movement = _currentVelocity * Time.fixedDeltaTime;
-        _rigidbody.MovePosition(_rigidbody.position + movement);
+        _rigidbody.linearVelocity = new Vector3(_currentVelocity.x, _rigidbody.linearVelocity.y, _currentVelocity.z);
     }
 }
