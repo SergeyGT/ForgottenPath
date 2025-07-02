85272d45c584904a88abad20e4ec711dfcf16238
SergeyGT <sergeyburdenyuk@mail.ru>
2025-07-02 12:34:22 +0300

    Дoбавлен скрипт для игрушки

---
+++ Assets/__Scripts/Puzzles/MusicPuzzle/Toy.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class Toy : MonoBehaviour, IInteractableObject
+{
+    [Header("Set Textures Toy")]
+    [SerializeField] private Material defaulMaterial;
+    [SerializeField] private Material highlightedMaterial;
+
+    [Header("Params of toy")]
+    [SerializeField] private int number;
+    [SerializeField] private AudioClip note;
+   
+    public Action<int> pressed;
+
+    private MeshRenderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        renderer.material = defaulMaterial;
+    }
+
+    public void Highlight(bool isHighlighted)
+    {
+        if (highlightedMaterial == null) return;
+
+        renderer.material = isHighlighted ? highlightedMaterial : defaulMaterial;
+    }
+
+    public void Interact()
+    {
+        
+        pressed?.Invoke(number);
+    }
+}
